231c48ed3463a7e1ff78058d789d8277b71b7c23
Sil3nceDev <juandiego.zerpa@hotmail.com>
2023-10-29 15:08:46 -0500

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,20 @@
 
 
 $(function () {
-"use strict";
-
-	//Countdown
-
-    $('[data-countdown]').each(function () {
-		var $this = $(this),
-			finalDate = $(this).data('countdown');
-		$this.countdown(finalDate, function (event) {
-			$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Days</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Hours</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
+	"use strict";
+	
+		//Countdown
+	
+		$('[data-countdown]').each(function () {
+			var $this = $(this),
+				finalDate = $(this).data('countdown');
+			$this.countdown(finalDate, function (event) {
+				$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Dias</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Horas</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutos</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Segundos</p></div></div>'));
+			});
 		});
-	});
-
-	// WOW active
-	new WOW().init();
-
-});	
-
+	
+		// WOW active
+		new WOW().init();
+	
+	});	
+	
